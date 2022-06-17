eaffec2606b42dd6ccd5d35f171c72d7caf156a4
Halevi, Asaf <asaf.halevi@gmail.com>
2022-06-17 18:05:58 +0300

    [0011] uchange timeout

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,6 +1,6 @@
 export const configuration = () => ({
   feedsDirectory: 'c:\\feeds',
-  alertTimeoutInSeconds: 6, //TODO - CHANGE TO 60
+  alertTimeoutInSeconds: 60,
   validUserIds: ['1424', '4323', '1194'],
   validFeedFileSuffix: '.batch',
   dbConnectionString: 'mongodb://localhost:27017/Vanti',
