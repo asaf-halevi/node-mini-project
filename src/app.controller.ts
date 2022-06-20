332828b4fa43b8c5e348da351cfdfa9717b4abc6
Halevi, Asaf <asaf.halevi@gmail.com>
2022-06-20 13:59:24 +0300

    [0013] auto start

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,11 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  startApplication() {
+  constructor(private readonly appService: AppService) {
     this.logger.log('App started');
     this.appService.lookForFeeds();
   }
